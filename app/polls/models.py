c2e4ca099fb02c17ba9f983aea76fcd176c6331a
DK <donald@noxqs.nl>
2017-04-25 21:18:13 +0200

    added method to model

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 class Question(models.Model):
     text = models.CharField(max_length=200)
@@ -8,7 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.text
 
-    
+    def published_recently?(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
