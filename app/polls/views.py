3a61bb17eeadfc4f1d35b337e2f0fa2c43dd298f
DK <donald@noxqs.nl>
2017-04-28 11:22:17 +0200

    Changed from relatedStringField to Choiizer

--- app/polls/views.py
+++ app/polls/views.py
@@ -2,8 +2,14 @@ from .models import Question, Choice
 from rest_framework import serializers, viewsets
 
 # serializers
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('id', 'text')
+
 class QuestionSerializer(serializers.ModelSerializer):
-    choices = serializers.StringRelatedField(many=True)
+    choices = ChoiceSerializer(many=True, read_only=True)
     class Meta:
         model = Question
         fields = ('id', 'text','pub_date','choices')
